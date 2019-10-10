36b80d7591cb4f91cbe6df0bae2eed513df617c5
Sergey Shyman <sergey@unisend.co>
2019-10-10 03:04:45 +0300

    test

---
+++ proxy/goproxy/signer.go
@@ -0,0 +1,136 @@
+package goproxy
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"math/big"
+	"math/rand"
+	"net"
+	"runtime"
+	"sort"
+	"time"
+	"sync"
+	"gitlab.com/NebulousLabs/fastrand"
+)
+
+func hashSorted(lst []string) []byte {
+	c := make([]string, len(lst))
+	copy(c, lst)
+	sort.Strings(c)
+	h := sha1.New()
+	for _, s := range c {
+		h.Write([]byte(s + ","))
+	}
+	return h.Sum(nil)
+}
+
+func hashSortedBigInt(lst []string) *big.Int {
+	rv := new(big.Int)
+	rv.SetBytes(hashSorted(lst))
+	return rv
+}
+
+var goproxySignerVersion = ":goroxy1"
+var hostMap sync.Map //map[string]*tls.Certificate
+
+func signHost(ca tls.Certificate, hosts []string) (cert *tls.Certificate, err error) {
+	var x509ca *x509.Certificate
+
+	// FIXME: There is a bug here. If hosts[] is ever more than one element long, there will be potential for host mismatches.
+	if len(hosts) == 0 {
+		return
+	}
+
+	cachedCert, ok := hostMap.Load(hosts[0])
+
+	if ok {
+		cert = cachedCert.(*tls.Certificate)
+		return
+	}
+
+	// Use the provided ca and not the global GoproxyCa for certificate generation.
+	if x509ca, err = x509.ParseCertificate(ca.Certificate[0]); err != nil {
+		return
+	}
+	start := time.Unix(0, 0)
+	end, err := time.Parse("2006-01-02", "2049-12-31")
+	if err != nil {
+		panic(err)
+	}
+
+	//serial := big.NewInt(rand.Int63())
+	hsh := make([]byte, 20)
+	fastrand.Read(hsh)
+	serial := new(big.Int)
+	serial.SetBytes(hsh)
+	template := x509.Certificate{
+		// TODO(elazar): instead of this ugly hack, just encode the certificate and hash the binary form.
+		SerialNumber: serial,
+		Issuer:       x509ca.Subject,
+		Subject: pkix.Name{
+			Organization: []string{"GoProxy untrusted MITM proxy Inc"},
+		},
+		NotBefore: start,
+		NotAfter:  end,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+	for _, h := range hosts {
+		if ip := net.ParseIP(h); ip != nil {
+			template.IPAddresses = append(template.IPAddresses, ip)
+		} else {
+			template.DNSNames = append(template.DNSNames, h)
+			template.Subject.CommonName = h
+		}
+	}
+
+	hash := hashSorted(append(hosts, goproxySignerVersion, ":"+runtime.Version()))
+	var csprng CounterEncryptorRand
+	if csprng, err = NewCounterEncryptorRandFromKey(ca.PrivateKey, hash); err != nil {
+		return
+	}
+
+	var certpriv crypto.Signer
+	switch ca.PrivateKey.(type) {
+	case *rsa.PrivateKey:
+		if certpriv, err = rsa.GenerateKey(&csprng, 2048); err != nil {
+			return
+		}
+	case *ecdsa.PrivateKey:
+		if certpriv, err = ecdsa.GenerateKey(elliptic.P256(), &csprng); err != nil {
+			return
+		}
+	default:
+		err = fmt.Errorf("unsupported key type %T", ca.PrivateKey)
+	}
+
+	var derBytes []byte
+	if derBytes, err = x509.CreateCertificate(&csprng, &template, x509ca, certpriv.Public(), ca.PrivateKey); err != nil {
+		return
+	}
+	tlsCert := &tls.Certificate{
+		Certificate: [][]byte{derBytes, ca.Certificate[0]},
+		PrivateKey:  certpriv,
+	}
+
+	// Cache the certificate for later.
+	for _, h := range hosts {
+		hostMap.Store(h, tlsCert)
+	}
+
+	return tlsCert, nil
+}
+
+func init() {
+	// Avoid deterministic random numbers
+	rand.Seed(time.Now().UnixNano())
+}
